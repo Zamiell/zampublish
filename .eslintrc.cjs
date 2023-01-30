8fc178692e33dde9a2820dbbc510782413adad3e
Zamiell <5511220+Zamiell@users.noreply.github.com>
2023-01-30 15:52:33 -0500

    chore: add files from zamts 0.0.43 template

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+// This is the configuration file for ESLint, the TypeScript linter:
+// https://eslint.org/docs/user-guide/configuring
+module.exports = {
+  extends: [
+    // The linter base is the shared IsaacScript config:
+    // https://github.com/IsaacScript/isaacscript/blob/main/packages/eslint-config-isaacscript/base.js
+    "eslint-config-isaacscript/base",
+  ],
+
+  parserOptions: {
+    // ESLint needs to know about the project's TypeScript settings in order for TypeScript-specific
+    // things to lint correctly. We do not point this at "./tsconfig.json" because certain files
+    // (such at this file) should be linted but not included in the actual project output.
+    project: "./tsconfig.eslint.json",
+  },
+
+  rules: {},
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# zampublish
+
+zampublish is a [TypeScript](https://www.typescriptlang.org/) project.
