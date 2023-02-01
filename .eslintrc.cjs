7f70bb7fe24739fa700651b4953c960fdd19dc37
James <5511220+Zamiell@users.noreply.github.com>
2023-01-31 23:43:26 -0500

    fix: lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,8 +7,9 @@ module.exports = {
     "eslint-config-isaacscript/base",
   ],
 
-  // Don't bother linting the compiled output.
-  ignorePatterns: ["**/dist/**"],
+  // The ".prettierrc.cjs" file is ignored by default, so we have to un-ignore it. Additionally, we
+  // don't bother linting the compiled output.
+  ignorePatterns: ["!.prettierrc.cjs", "**/dist/**"],
 
   parserOptions: {
     // ESLint needs to know about the project's TypeScript settings in order for TypeScript-specific
